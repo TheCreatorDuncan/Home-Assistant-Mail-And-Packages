d30946f55c595b41fc380c47a0d5bf547837224a
TheCreatorDuncan <117076573+TheCreatorDuncan@users.noreply.github.com>
2026-04-02 20:57:43 +0200

    Update sensor.py
    Updated to match const.py

--- custom_components/mail_and_packages/sensor.py
+++ custom_components/mail_and_packages/sensor.py
@@ -5,32 +5,23 @@ Configuration code contribution from @firstof9 https://github.com/firstof9/
 """
 import datetime
 from datetime import timezone
-import logging
 from typing import Any, Optional
 
 from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, CONF_RESOURCES
-from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import (
     AMAZON_EXCEPTION_ORDER,
     AMAZON_ORDER,
-    ATTR_IMAGE,
-    ATTR_IMAGE_NAME,
-    ATTR_IMAGE_PATH,
     ATTR_ORDER,
     ATTR_TRACKING_NUM,
-    CONF_PATH,
     COORDINATOR,
     DOMAIN,
-    IMAGE_SENSORS,
     SENSOR_TYPES,
     VERSION,
 )
-
-_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
@@ -40,10 +31,8 @@ async def async_setup_entry(hass, entry, async_add_entities):
     resources = entry.data[CONF_RESOURCES]
 
     for variable in resources:
-        sensors.append(PackagesSensor(entry, SENSOR_TYPES[variable], coordinator))
-
-    for variable, value in IMAGE_SENSORS.items():
-        sensors.append(ImagePathSensors(hass, entry, value, coordinator))
+        if variable in SENSOR_TYPES:
+            sensors.append(PackagesSensor(entry, SENSOR_TYPES[variable], coordinator))
 
     async_add_entities(sensors, False)
 
@@ -55,7 +44,7 @@ class PackagesSensor(CoordinatorEntity, SensorEntity):
         self,
         config: ConfigEntry,
         sensor_description: SensorEntityDescription,
-        coordinator: str,
+        coordinator,
     ):
         """Initialize the sensor."""
         super().__init__(coordinator)
@@ -91,15 +80,11 @@ class PackagesSensor(CoordinatorEntity, SensorEntity):
     @property
     def native_value(self) -> Any:
         """Return the state of the sensor."""
-        value = None
-
-        if self.type in self.coordinator.data.keys():
-            value = self.coordinator.data[self.type]
+        if self.type in self.coordinator.data:
             if self.type == "mail_updated":
-                value = datetime.datetime.now(timezone.utc)
-        else:
-            value = None
-        return value
+                return datetime.datetime.now(timezone.utc)
+            return self.coordinator.data[self.type]
+        return None
 
     @property
     def should_poll(self) -> bool:
@@ -112,105 +97,28 @@ class PackagesSensor(CoordinatorEntity, SensorEntity):
         return self.coordinator.last_update_success
 
     @property
-    def extra_state_attributes(self) -> Optional[str]:
+    def extra_state_attributes(self) -> Optional[dict]:
         """Return device specific state attributes."""
         attr = {}
-        tracking = f"{'_'.join(self.type.split('_')[:-1])}_tracking"
         data = self.coordinator.data
 
-        # Catch no data entries
         if self.data is None:
             return attr
 
-        if "Amazon" in self._name:
-            if self._name == "amazon_exception":
+        # Tracking key based on sensorname
+        tracking = f"{'_'.join(self.type.split('_')[:-1])}_tracking"
+
+        # Amazon order attributes
+        if self.type == "amazon_exception":
+            if AMAZON_EXCEPTION_ORDER in data:
                 attr[ATTR_ORDER] = data[AMAZON_EXCEPTION_ORDER]
-            else:
+
+        elif self.type.startswith("amazon_"):
+            if AMAZON_ORDER in data:
                 attr[ATTR_ORDER] = data[AMAZON_ORDER]
-        elif self._name == "Mail USPS Mail":
-            attr[ATTR_IMAGE] = data[ATTR_IMAGE_NAME]
-        elif "_delivering" in self.type and tracking in self.data.keys():
+
+        # For all *_delivering sensors add tracking if applicable
+        if self.type.endswith("_delivering") and tracking in data:
             attr[ATTR_TRACKING_NUM] = data[tracking]
-            # TODO: Add Tracking URL when applicable
+
         return attr
-
-
-class ImagePathSensors(CoordinatorEntity, SensorEntity):
-    """Representation of a sensor."""
-
-    def __init__(
-        self,
-        hass: HomeAssistant,
-        config: ConfigEntry,
-        sensor_description: SensorEntityDescription,
-        coordinator: str,
-    ):
-        """Initialize the sensor."""
-        super().__init__(coordinator)
-        self.entity_description = sensor_description
-        self.hass = hass
-        self.coordinator = coordinator
-        self._config = config
-        self._name = sensor_description.name
-        self.type = sensor_description.key
-        self._host = config.data[CONF_HOST]
-        self._unique_id = self._config.entry_id
-
-    @property
-    def device_info(self) -> dict:
-        """Return device information about the mailbox."""
-        return {
-            "connections": {(DOMAIN, self._unique_id)},
-            "name": self._host,
-            "manufacturer": "IMAP E-Mail",
-            "sw_version": VERSION,
-        }
-
-    @property
-    def unique_id(self) -> str:
-        """Return a unique, Home Assistant friendly identifier for this entity."""
-        return f"{self._host}_{self._name}_{self._unique_id}"
-
-    @property
-    def name(self) -> str:
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
-    def native_value(self) -> Optional[str]:
-        """Return the state of the sensor."""
-        image = self.coordinator.data[ATTR_IMAGE_NAME]
-        the_path = None
-
-        if ATTR_IMAGE_PATH in self.coordinator.data.keys():
-            path = self.coordinator.data[ATTR_IMAGE_PATH]
-        else:
-            path = self._config.data[CONF_PATH]
-
-        if self.type == "usps_mail_image_system_path":
-            _LOGGER.debug("Updating system image path to: %s", path)
-            the_path = f"{self.hass.config.path()}/{path}{image}"
-        elif self.type == "usps_mail_image_url":
-            if (
-                self.hass.config.external_url is None
-                and self.hass.config.internal_url is None
-            ):
-                the_path = None
-            elif self.hass.config.external_url is None:
-                _LOGGER.warning("External URL not set in configuration.")
-                url = self.hass.config.internal_url
-                the_path = f"{url.rstrip('/')}/local/mail_and_packages/{image}"
-            else:
-                url = self.hass.config.external_url
-                the_path = f"{url.rstrip('/')}/local/mail_and_packages/{image}"
-        return the_path
-
-    @property
-    def should_poll(self) -> bool:
-        """No need to poll. Coordinator notifies entity of updates."""
-        return False
-
-    @property
-    def available(self) -> bool:
-        """Return if entity is available."""
-        return self.coordinator.last_update_success
